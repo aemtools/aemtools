2647b1c4db96a41ef8ffe00f04a8083e77b0494c
Dmytro Troynikov <dmytrotroynikov@gmail.com>
2017-02-23 22:52:43 +0200

    #5 documentation for options

--- src/main/kotlin/com/aemtools/documentation/htl/HtlELDocumentationProvider.kt
+++ src/main/kotlin/com/aemtools/documentation/htl/HtlELDocumentationProvider.kt
@@ -1,8 +1,12 @@
 package com.aemtools.documentation.htl
 
+import com.aemtools.completion.util.findParentByType
+import com.aemtools.lang.htl.psi.pattern.HtlPatterns.contextOptionAssignment
 import com.aemtools.lang.htl.psi.pattern.HtlPatterns.optionName
+import com.aemtools.service.repository.inmemory.HtlAttributesRepository
 import com.intellij.lang.documentation.AbstractDocumentationProvider
 import com.intellij.psi.PsiElement
+import generated.psi.impl.HtlStringLiteralImpl
 
 /**
  *
@@ -11,12 +15,27 @@ import com.intellij.psi.PsiElement
 open class HtlELDocumentationProvider : AbstractDocumentationProvider() {
 
     override fun generateDoc(element: PsiElement?, originalElement: PsiElement?): String? {
-        if (originalElement != null && optionName.accepts(originalElement)) {
-            val text = originalElement.text
-            // todo generate doc
-        }
+        val text = originalElement?.text ?: return super.generateDoc(element, originalElement)
+        return when {
+            optionName.accepts(originalElement) -> {
+                HtlAttributesRepository.getHtlOptions().find {
+                    it.name == text
+                }?.let(HtlAttributesRepository.HtlOption::description)
+                        ?: super.generateDoc(element, originalElement)
 
-        return super.generateDoc(element, originalElement)
+            }
+            contextOptionAssignment.accepts(originalElement) -> {
+                val literal = originalElement.findParentByType(HtlStringLiteralImpl::class.java)
+                        ?: return super.generateDoc(element, originalElement)
+                val stringValue = literal.name
+
+                HtlAttributesRepository.getContextValues().find {
+                    it.name == stringValue
+                }?.let(HtlAttributesRepository.HtlContextValue::description)
+                        ?: super.generateDoc(element, originalElement)
+            }
+            else -> super.generateDoc(element, originalElement)
+        }
     }
 
 }
