4532a5a32e3e57a81c98019a663d0dda0f215884
Dmytro Troynikov <dmytrotroynikov@gmail.com>
2017-03-14 23:29:46 +0200

    #62 fixed find usage for methods referenced by short name

---
+++ src/main/kotlin/com/aemtools/findusages/HtlPropertyScopeProvider.kt
@@ -0,0 +1,19 @@
+package com.aemtools.findusages
+
+import com.intellij.openapi.fileTypes.StdFileTypes
+import com.intellij.openapi.project.Project
+import com.intellij.psi.impl.search.CustomPropertyScopeProvider
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.SearchScope
+
+/**
+ * Enables *Find Usages* action to search in HTL files.
+ * @author Dmytro Troynikov
+ */
+class HtlPropertyScopeProvider : CustomPropertyScopeProvider {
+    override fun getScope(project: Project): SearchScope =
+            GlobalSearchScope.getScopeRestrictedByFileTypes(
+                    GlobalSearchScope.projectScope(project),
+                    StdFileTypes.HTML
+            )
+}
