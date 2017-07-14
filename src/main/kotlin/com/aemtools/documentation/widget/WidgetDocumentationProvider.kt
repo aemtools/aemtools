f2caca63195a08d5e4249e6f04622d0e6d5f635a
Dmytro_Troynikov <dmytrotroynikov@gmail.com>
2017-07-14 12:38:18 +0300

    #72 fix test

--- src/main/kotlin/com/aemtools/documentation/widget/WidgetDocumentationProvider.kt
+++ src/main/kotlin/com/aemtools/documentation/widget/WidgetDocumentationProvider.kt
@@ -51,17 +51,29 @@ open class WidgetDocumentationProvider : AbstractDocumentationProvider() {
         return documentation
     }
 
-    fun xtypeDocumentation(widgetDoc: WidgetDoc): String {
-        return """
+    /**
+     * Generate documentation for `xtype`.
+     *
+     * @param widgetDoc the xtype data holder
+     *
+     * @return string with xtype documentation
+     */
+    fun xtypeDocumentation(widgetDoc: WidgetDoc): String = """
             <h2>${widgetDoc.className}</h2>
             <p>
                 ${widgetDoc.description}
             </p>
         """.trimIndent().replace("\n", "")
-    }
 
-    fun fieldDocumentation(widgetDoc: WidgetDoc, widgetMember: WidgetMember): String {
-        return """
+    /**
+     * Generate documentation for specific member of `xtype` (field, method).
+     *
+     * @param widgetDoc the xtype descriptor
+     * @param widgetMember the specific widget member
+     *
+     * @return string with documentation
+     */
+    fun fieldDocumentation(widgetDoc: WidgetDoc, widgetMember: WidgetMember): String = """
             <h2>${widgetDoc.fullClassName}</h2>
             <p>
                 Field name: <b>${widgetMember.name}</b>
@@ -73,6 +85,5 @@ open class WidgetDocumentationProvider : AbstractDocumentationProvider() {
                 ${widgetMember.description}
             </p>
         """.trimIndent().replace("\n", "")
-    }
 
 }
