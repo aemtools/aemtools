9c120df4a474734a16a512eb47a0227322a63da1
Dmytro_Troynikov <dmytrotroynikov@gmail.com>
2017-08-29 15:26:18 +0300

    #111 code refactorings

--- src/main/kotlin/com/aemtools/reference/htl/provider/DataSlyIncludeElReferenceProvider.kt
+++ src/main/kotlin/com/aemtools/reference/htl/provider/DataSlyIncludeElReferenceProvider.kt
@@ -1,26 +1,18 @@
 package com.aemtools.reference.htl.provider
 
-import com.aemtools.index.HtlIndexFacade
 import com.aemtools.lang.htl.psi.mixin.HtlStringLiteralMixin
-import com.aemtools.reference.common.reference.PsiFileReference
-import com.intellij.openapi.util.TextRange
+import com.aemtools.reference.common.provider.DataSlyIncludeReferenceProviderBase
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiReference
-import com.intellij.psi.PsiReferenceProvider
 import com.intellij.util.ProcessingContext
 
 /**
  * @author Dmytro_Troynikov
  */
-object DataSlyIncludeElReferenceProvider : PsiReferenceProvider() {
-    override fun getReferencesByElement(element: PsiElement, context: ProcessingContext): Array<out PsiReference> {
-        val value = element as HtlStringLiteralMixin
-        val psiFile = HtlIndexFacade.resolveIncludeFile(value.name, value.containingFile)
+object DataSlyIncludeElReferenceProvider : DataSlyIncludeReferenceProviderBase() {
 
-        return if (psiFile != null) {
-            arrayOf(PsiFileReference(psiFile, value, TextRange(1, value.textLength - 1)))
-        } else {
-            arrayOf()
-        }
+    override fun name(element: PsiElement, context: ProcessingContext): String? {
+        val literal = element as? HtlStringLiteralMixin
+        return literal?.name
     }
+
 }
