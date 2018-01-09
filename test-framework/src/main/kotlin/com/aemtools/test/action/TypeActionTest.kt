3774070c628d9d89df7040afcf0e4d087f2cb27c
Dmytro_Troynikov <dmytrotroynikov@gmail.com>
2018-01-09 19:32:37 +0200

    #0 htl backspace handler

---
+++ test-framework/src/main/kotlin/com/aemtools/test/action/TypeActionTest.kt
@@ -0,0 +1,31 @@
+package com.aemtools.test.action
+
+import com.aemtools.test.base.BaseLightTest
+
+/**
+ * @author Dmytro Troynikov
+ */
+interface TypeActionTest {
+
+  /**
+   * One file based type action test.
+   *
+   * @param fileName name of input file
+   * @param before "before" state of file
+   * @param type the string to type
+   * @param result "after" state of file
+   * @see BasePlatformLightTest,
+   */
+  fun BaseLightTest.typedActionTest(fileName: String,
+                                    before: String,
+                                    type: String,
+                                    result: String) {
+
+    fixture.apply {
+      configureByText(fileName, before)
+      type(type)
+      checkResult(result)
+    }
+  }
+
+}
