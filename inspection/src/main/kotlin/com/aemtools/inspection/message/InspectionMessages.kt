cf123394dc6918fa8c68d3b412290eb6a8cff6b2
Dmytro Troynikov <dmytrotroynikov@gmail.com>
2018-02-21 01:26:57 +0200

    #124 implemented

--- inspection/src/main/kotlin/com/aemtools/inspection/message/InspectionMessages.kt
+++ inspection/src/main/kotlin/com/aemtools/inspection/message/InspectionMessages.kt
@@ -4,5 +4,5 @@ package com.aemtools.inspection.message
  * @author Dmytro Troynikov
  */
 object InspectionMessages {
-  val REDUNDANT_DATA_SLY_UNWRAP_MESSAGE = "The data-sly-unwrap attribute is redundant in sly tag."
+  const val REDUNDANT_DATA_SLY_UNWRAP_MESSAGE = "The data-sly-unwrap attribute is redundant in sly tag."
 }
