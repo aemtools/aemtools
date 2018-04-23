ba2e60bb2436c3be776bba06b6f9709891e6235c
Dmytro Primshyts <dprimshyts@gmail.com>
2018-04-24 01:11:40 +0300

    #133 jcr type actions

---
+++ aem-intellij-core/src/main/kotlin/com/aemtools/completion/small/reppolicy/RepPolicyCompletionContributor.kt
@@ -0,0 +1,11 @@
+package com.aemtools.completion.small.reppolicy
+
+import com.intellij.codeInsight.completion.CompletionContributor
+
+/**
+ * @author Dmytro Primshyts.
+ */
+class RepPolicyCompletionContributor : CompletionContributor() {init {
+
+}
+}
