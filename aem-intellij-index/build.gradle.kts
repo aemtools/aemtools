9349c39f9a881fb6e9543a2ec3bf0cb29095a80f
Arsen Retiznyk <arsen.retiznyk@gmail.com>
2018-05-21 23:18:13 +0300

    #0 moved client library index to a separate module

--- aem-intellij-index/build.gradle.kts
+++ aem-intellij-index/build.gradle.kts
@@ -1,15 +1,20 @@
+import org.jetbrains.intellij.IntelliJPlugin
 import org.jetbrains.intellij.IntelliJPluginExtension
-import org.jetbrains.intellij.tasks.RunIdeTask
-import org.jetbrains.kotlin.cli.jvm.main
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.utils.identity
 
 buildscript {
     val kotlinVersion: String by extra
 
     repositories {
         mavenCentral()
+        jcenter()
+        mavenLocal()
+        maven {
+            setUrl("http://dl.bintray.com/jetbrains/intellij-plugin-service")
+        }
     }
-    
+
     dependencies {
         classpath(kotlin("gradle-plugin", kotlinVersion))
     }
@@ -18,7 +23,7 @@ buildscript {
 apply {
     plugin("java")
     plugin("kotlin")
-    plugin("org.junit.platform.gradle.plugin")
+    plugin("org.jetbrains.intellij")
 }
 
 plugins {
@@ -26,35 +31,18 @@ plugins {
     id("org.jetbrains.intellij") version "0.3.0"
 }
 
-val kotlinVersion: String by extra
-val gsonVersion: String by extra
-val apacheCommonsVersion: String by extra
-
 dependencies {
     compile(project(":aem-intellij-lang"))
     compile(project(":aem-intellij-common"))
-    compile(project(":aem-intellij-inspection"))
-    compile(project(":aem-intellij-index"))
-    compile(project(":aem-intellij-integration"))
-
-    compile("com.google.code.gson:gson:$gsonVersion")
-    compile("org.apache.commons:commons-lang3:$apacheCommonsVersion")
-
     testCompile(project(":test-framework"))
 }
 
 val ideaVersion: String by extra
 
 configure<IntelliJPluginExtension> {
-    pluginName = "aemtools"
     version = ideaVersion
-    downloadSources = true
-    updateSinceUntilBuild = false
+
     setPlugins(
             "IntelliLang"
     )
-}
-
-tasks.withType<RunIdeTask> {
-    jvmArgs.add("-Didea.ProcessCanceledException=disabled")
 }
