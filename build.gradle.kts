86d0ff516c1ea5bd7cf7994ff5df8f73135400ea
koha26 <konstantyn.diachenko@gmail.com>
2026-08-13 17:22:18 -0600

    Use pluginComposedModule for fix an artifact structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -155,11 +155,11 @@ dependencies {
     bundledPlugins(platformBundledPlugins)
     plugins(platformPlugins)
 
-    pluginModule(implementation(project(":aem-intellij-common")))
-    pluginModule(implementation(project(":aem-intellij-core")))
-    pluginModule(implementation(project(":aem-intellij-lang")))
-    pluginModule(implementation(project(":aem-intellij-inspection")))
-    pluginModule(implementation(project(":aem-intellij-index")))
+    pluginComposedModule(implementation(project(":aem-intellij-common")))
+    pluginComposedModule(implementation(project(":aem-intellij-core")))
+    pluginComposedModule(implementation(project(":aem-intellij-lang")))
+    pluginComposedModule(implementation(project(":aem-intellij-inspection")))
+    pluginComposedModule(implementation(project(":aem-intellij-index")))
 
     testFramework(TestFrameworkType.Platform, configurationName = Constants.Configurations.INTELLIJ_PLATFORM_DEPENDENCIES)
     testFramework(TestFrameworkType.Plugin.Java, configurationName = Constants.Configurations.INTELLIJ_PLATFORM_DEPENDENCIES)
